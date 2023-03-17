9fdb9ddf887172885f6e5d1c65ccd750acf9dfce
Ricardo <richy.2603@gmail.com>
2023-03-17 15:19:58 -0500

    Agregamos elementos  y la estructura

---
+++ src/Layout/GeneralLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from "antd";
+
+export const GeneralLayout = () => {
+  return (
+    <Layout>
+      <h2>Menú</h2>
+      <div>Contenido</div>
+      <h2>Footer</h2>
+    </Layout>
+  )
+}
